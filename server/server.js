174b94f6faaa3b7195a494782248bcd64c35e012
anilson <anilsonmonteiro@gmail.com>
2020-08-02 19:39:03 -0100

    add updates to server code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,41 @@
+require('dotenv').config();
 const WebSocket = require('ws');
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const path = require('path');
 
-const wss = new WebSocket.Server({port: 888});
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+
+const server = http.createServer(app);
+
+const port = process.env.PORT;
+
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', function (ws) {
+    const id = setInterval(function () {
+      ws.send(JSON.stringify(process.memoryUsage()), function () {
+        //
+        // Ignore errors.
+        //
+      });
+    }, 100);
+    console.log('started client interval');
+  
+    ws.on('close', function () {
+      console.log('stopping client interval');
+      clearInterval(id);
+    });
+  });
 
 wss.on('connection', function connection(ws){
     ws.on('message', function incoming(event){
@@ -11,3 +46,21 @@ wss.on('connection', function connection(ws){
 
     ws.send(`message received at: ${timeReceived}`)
 });
+
+
+server.listen(port, function () {
+    console.log(`Listening on http://localhost:${port}`);
+  });
+
+ /*
+const express = require('express')
+const app = express()
+const port = 4001
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(0, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})*/
